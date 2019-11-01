49f2186cece84b2ed1f282356d5779163781d6d5
vinu tv <vinu@sv.co>
2019-11-01 17:51:07 +0530

    Add gatsby portfolio theme

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://g3mo.github.io/gemo/',
+  pathPrefix: '/',
+  title: 'Gautham',
+  subtitle: 'I can’t help but write. It’s the only way I grok.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    },
+    {
+      label: 'New Page',
+      path: '/pages/new'
+    }
+  ],
+  author: {
+    name: 'Gautham',
+    photo: '/photo.jpg',
+    bio: 'I can’t help but write. It’s the only way I grok.',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: ''
+    }
+  }
+};
--- contact.md
+++
@@ -1,8 +0,0 @@
-**Contact**
-
-
-The best way to reach me is via [email](mailto:gauthamail@gmail.com)
-
-I occasionally tweet, and I'm at [@g3Mo](https://twitter.com/g3Mo) on Twitter
-
-Last updated : 28 October 2019
