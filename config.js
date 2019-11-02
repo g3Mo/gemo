bf69f9d396fda1e7ebfa31cf0897d6c5e3ab9f78
vinu tv <vinu@sv.co>
2019-11-02 17:15:43 +0530

    Update photo and minor style fixes

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   pathPrefix: '/gemo',
   title: 'Gautham',
   subtitle: 'I can’t help but write. It’s the only way I grok.',
-  
+  copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
@@ -23,14 +23,24 @@ module.exports = {
       label: 'Contact me',
       path: '/pages/contacts'
     },
-      ],
+  ],
   author: {
     name: 'Gautham',
     photo: '/photo.jpg',
     bio: 'I can’t help but write. It’s the only way I grok.',
     contacts: {
-      email:gauthamail@gmail.com '',
-      twitter:twitter.com/g3mo '#',
+      email: 'gauthamail@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: 'twitter.com/g3mo',
+      github: '',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: ''
     }
   }
 };
