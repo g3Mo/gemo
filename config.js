23ba0a8cf9bc50a21a97aad17d8a9ec83751d5cd
Gautham <gautham@mobme.in>
2019-11-02 14:44:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   pathPrefix: '/gemo',
   title: 'Gautham',
   subtitle: 'I can’t help but write. It’s the only way I grok.',
-  copyright: '© All rights reserved.',
+  
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
@@ -23,28 +23,14 @@ module.exports = {
       label: 'Contact me',
       path: '/pages/contacts'
     },
-    {
-      label: 'New Page',
-      path: '/pages/new'
-    }
-  ],
+      ],
   author: {
     name: 'Gautham',
     photo: '/photo.jpg',
     bio: 'I can’t help but write. It’s the only way I grok.',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: ''
+      email:gauthamail@gmail.com '',
+      twitter:twitter.com/g3mo '#',
     }
   }
 };
