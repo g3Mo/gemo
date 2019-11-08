9fb4a592e3885b755d605784c0e5df2b0292e771
Gautham <Gemo@Gauthams-MacBook-Pro.local>
2019-11-08 15:12:05 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-25fb9989b30fb9476dd5.js"
+    "url": "webpack-runtime-5e712fa15375eef5732e.js"
   },
   {
     "url": "styles.6d1016d282fed8788e2e.css"
@@ -36,17 +36,21 @@ self.__precacheManifest = [
     "url": "styles-c54d467e3152b7dbde15.js"
   },
   {
-    "url": "commons-946281833bc279afac14.js"
+    "url": "commons-df008171a2a01e4bf488.js"
   },
   {
-    "url": "app-8879c8d30aa6862dba34.js"
+    "url": "app-f2a867ca8ec65f544620.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-a7388efdc62430243a75.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c2897d0798619072a7c9fa2bc498d3f4"
+    "revision": "ecccaddea3ca66bba2647dfa6701dbc2"
+  },
+  {
+    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
+    "revision": "d274adf0f008ef152ce70a312b04b730"
   },
   {
     "url": "manifest.json",
@@ -77,12 +81,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   }
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
+  pathname = pathname.replace(new RegExp(`^/blog`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-8879c8d30aa6862dba34.js`))) {
+  if (!resources || !(await caches.match(`/blog/app-f2a867ca8ec65f544620.js`))) {
     return await fetch(event.request)
   }
 
@@ -95,7 +99,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/blog/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
